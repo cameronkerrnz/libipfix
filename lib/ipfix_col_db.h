3e1c5d4c6121344f97ea68435d1d3499e2b78294
Cameron Kerr <cameron.kerr.nz@gmail.com>
2015-03-05 16:38:49 +1300

    Remove misplaced declaration of ipfix_export_drecord_jsonfile

--- lib/ipfix_col_db.h
+++ lib/ipfix_col_db.h
@@ -22,8 +22,6 @@ int  ipfix_export_trecord_db( ipfixs_node_t *s, ipfixt_node_t *t, void *arg );
 int  ipfix_export_drecord_db( ipfixs_node_t *s, ipfixt_node_t *t,
                               ipfix_datarecord_t *d, void *arg,
                               ipfix_input_t      *source );
-int  ipfix_export_drecord_jsonfile( ipfixs_node_t *s, ipfixt_node_t *t,
-                                     ipfix_datarecord_t *d, void *arg );
 void ipfix_export_cleanup_db( void *arg );
 int  ipfix_export_init_db( char *dbhost, char *dbuser,
                            char *dbpw, char *dbname, 
